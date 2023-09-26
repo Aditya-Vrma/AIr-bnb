f2869bd489e1181d7e285b2b46d4f95ac742329c
Aditya-Vrma <vermaaditya77777@gmail.com>
2023-09-26 18:01:04 +0530

    API work starts here

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom"; 
+import { Route, Routes } from "react-router-dom";
 import IndexPage from "./Pages/IndexPage";
 import LoginPage from "./Pages/LoginPage";
+import RegisterPage from "./Pages/RegisterPage";
 import Layout from "./Layout";
-
 function App() {
-  return (  
+  return (
     <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<IndexPage  />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
