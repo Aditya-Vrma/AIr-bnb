56b7ea3a4badb74fb7ef6435d607397bb1e793fc
Aditya-Vrma <vermaaditya77777@gmail.com>
2023-10-01 23:20:22 +0530

    All errors resorted till Login Page

--- api/index.js
+++ api/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const User = require("./models/User.js");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
-
-const bcryptsalt = bcrypt.genSaltSync(10); // we can also added await here but we add Sync in genSalt
 
 app.use(
   cors({
@@ -15,6 +13,8 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
+
+const bcryptsalt = bcrypt.genSaltSync(10); // we can also added await here but we add Sync in genSalt
 
 console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL);
@@ -31,11 +31,10 @@ app.post("/register", async (req, res) => {
       email,
       password: bcrypt.hashSync(password, bcryptsalt), //to add encription to our password , we are adding node package called bcrypyjs
     });
+    res.json(userDoc);
   } catch (e) {
     res.status(422).json(e);
   }
-
-  res.json(userDoc);
 });
 
 app.listen(4000, () => {
